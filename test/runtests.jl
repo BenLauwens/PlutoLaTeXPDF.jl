4d84d25edd2bcdac1dacd8a5905dec4e2dfa7f2a
Ben Lauwens <ben.lauwens@gmail.com>
2022-11-28 21:26:33 +0100

    Font update

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,13 @@ using Test
     PlutoLaTeXPDF.ADMONITIONTYPES["languages"] = ("awesomeblock", raw"[ForestGreen]{2pt}{\faCode}{ForestGreen}")
     dir = joinpath(@__DIR__, "..", "example")
     file = joinpath(@__DIR__, "notebooks", "test_1.jl")
-    file = joinpath(homedir(), "GitHub", "ES123", "Lectures", "Lecture07.jl")
+    file = joinpath(homedir(), "GitHub", "ES123", "Lectures", "Lecture08.jl")
     #toPDF(dir, file; author="Ben Lauwens")
     files = String[]
     for i in 1:7
         push!(files, joinpath(homedir(), "GitHub", "ES123", "Lectures", "Lecture0$i.jl"))
     end
-    ##=
+    ###=
     toPDF(joinpath(@__DIR__, "..", "example", "book.pdf"), files;
                title="ThinkJulia, How to Think as a Computer Scientist",
                author="Ben Lauwens",
